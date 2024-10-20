d50ad0815b8c0978b0c19d58d8588db13a68e25b
haoosz <haoshaozhe@gmail.com>
2024-10-21 02:14:14 +0800

    update

--- script/train.sh
+++ script/train.sh
@@ -1,4 +1,4 @@
-TORCH_DISTRIBUTED_DEBUG=INFO TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 torchrun --nnodes=1 --nproc_per_node=2 train_ddp.py \
+TORCH_DISTRIBUTED_DEBUG=INFO TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 torchrun --nnodes=1 --nproc_per_node=8 train_ddp.py \
  --ckpt_bae ckpts/bae/bae_d24/binaryae_ema_1000000.th \
  --dataset custom --codebook_size 24 --img_size 256 --norm_first \
  --model BiGR-L \
